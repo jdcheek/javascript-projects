886ecbf58ce4931c58ad37806c3c9d7dc7c0119a
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-01-10 22:37:20 -0600

    Completed debugging exercises

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -5,6 +5,7 @@
 let launchReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
+let crewReady = true;
 let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
@@ -19,10 +20,10 @@ console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("crewReady = ", crewReady);
