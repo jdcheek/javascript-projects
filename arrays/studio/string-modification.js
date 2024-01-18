1dc06004765b40423199ad78a5149804724e7cf4
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-01-17 21:46:04 -0600

    chapter 8 studio part 1

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,10 +1,18 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
+let userInput = input.question("Choose the number of letters to be relocated ") || 3
+
+while (userInput > str.length || userInput < 0) {
+    userInput = input.question(`Choose the number of letters to be relocated. (Must be a positive number less than or equal to  ${str.length} `) || 3
+}
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let slice = str.slice(userInput)
+let newStr = slice + str.slice(0,userInput)
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`${newStr} used to say ${str}`)
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
