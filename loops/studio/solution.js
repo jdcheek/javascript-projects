a3d2af633c818cc28ce8678b97dc2c4631a8f186
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-01-21 20:46:51 -0600

    studio part 1

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -15,13 +15,16 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
 
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
+  for (let i = 0; i < numMeals; i++) {
+    meals.push([protein[i], grains[i], veggies[i], beverages[i], desserts[i]]);
+  }
 
   return meals;
 }
 
 
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
+  let numMeals = input.question("How many meals would you like to make? ");
 
   /// CODE YOUR SOLUTION TO PART B here ///
 
@@ -43,17 +46,16 @@ function runProgram() {
   /// UNCOMMENT the two lines of code below that invoke the mealAssembly function (starting with 'let meals =') and print the result ///
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
-
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(meals)
 
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
 
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
   /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
