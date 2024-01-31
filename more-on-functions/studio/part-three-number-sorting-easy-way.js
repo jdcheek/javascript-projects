2ef68835440aff5e7abc55c7f96be2cd3cec6755
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-01-31 17:55:38 -0600

    chapter 11 studio part 3

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -5,6 +5,7 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
+
 console.log(
   nums1.sort(function(a, b) {
     return a - b
@@ -21,6 +22,7 @@ console.log(
   })
 )
 //Sort each array in decending order.
+
 console.log(
   nums1.sort(function(a, b) {
     return b - a
