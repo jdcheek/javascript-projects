ab1b0761aa1b2f6ac4833a3ad06dcd99d2a9020b
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-02-01 20:42:15 -0600

    chapter 12 studio

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,55 +1,79 @@
-// Code your selectRandomEntry function here:
-
-
-// Code your buildCrewArray function here:
-
-
 let idNumbers = [291, 414, 503, 599, 796, 890];
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
-  'name':'Gordon Shumway',
-  'species':'alf',
-  'mass':90,
-  'o2Used':function(hrs){return 0.035*hrs},
-  'astronautID':414
+  'name': 'Gordon Shumway',
+  'species': 'alf',
+  'mass': 90,
+  'o2Used': function(hrs) { return 0.035 * hrs },
+  'astronautID': 414
 };
 let candidateB = {
-  'name':'Lassie',
-  'species':'dog',
-  'mass':19.1,
-  'o2Used':function(hrs){return 0.030*hrs},
-  'astronautID':503
+  'name': 'Lassie',
+  'species': 'dog',
+  'mass': 19.1,
+  'o2Used': function(hrs) { return 0.030 * hrs },
+  'astronautID': 503
 };
 let candidateC = {
-  'name':'Jonsey',
-  'species':'cat',
-  'mass':3.6,
-  'o2Used':function(hrs){return 0.022*hrs},
-  'astronautID':796
+  'name': 'Jonsey',
+  'species': 'cat',
+  'mass': 3.6,
+  'o2Used': function(hrs) { return 0.022 * hrs },
+  'astronautID': 796
 };
 let candidateD = {
-  'name':'Paddington',
-  'species':'bear',
-  'mass':31.8,
-  'o2Used':function(hrs){return 0.047*hrs},
-  'astronautID':291
+  'name': 'Paddington',
+  'species': 'bear',
+  'mass': 31.8,
+  'o2Used': function(hrs) { return 0.047 * hrs },
+  'astronautID': 291
 };
 let candidateE = {
-  'name':'Pete',
-  'species':'tortoise',
-  'mass':417,
-  'o2Used':function(hrs){return 0.010*hrs},
-  'astronautID':599
+  'name': 'Pete',
+  'species': 'tortoise',
+  'mass': 417,
+  'o2Used': function(hrs) { return 0.010 * hrs },
+  'astronautID': 599
 };
 let candidateF = {
-  'name':'Hugs',
-  'species':'ball python',
-  'mass':2.3,
-  'o2Used':function(hrs){return 0.018*hrs},
-  'astronautID':890
+  'name': 'Hugs',
+  'species': 'ball python',
+  'mass': 2.3,
+  'o2Used': function(hrs) { return 0.018 * hrs },
+  'astronautID': 890
 };
 
-let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+let animals = [candidateA, candidateB, candidateC, candidateD, candidateE, candidateF];
 
-// Code your template literal and console.log statements:
+// Code your selectRandomEntry function here:
+function selectRandomEntry(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+let crewIdArr = []
+
+while (crewIdArr.length < 3) {
+  let id = selectRandomEntry(idNumbers)
+  if (!crewIdArr.includes(id)) {
+    crewIdArr.push(id)
+  }
+}
+
+console.log(crewIdArr)
+
+let crew = []
+
+function buildCrewArray(crewIdArr, animals) {
+  let i = 0
+  while (crew.length < 3) {
+    if (crewIdArr.includes(animals[i]['astronautID'])) {
+      crew.push(animals[i]['name'])
+    }
+    i++
+  }
+}
+
+buildCrewArray(crewIdArr, animals)
+console.log(crew)
+// Code your template liateral and console.log statements:
--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,58 +1,70 @@
 // Code your orbitCircumference function here:
+function orbitCircumference(altitude) {
+  return Math.round(2 * Math.PI * altitude)
+}
 
 
 // Code your missionDuration function here:
-
+function missionDuration(numberOfOrbits, orbitRadius, orbitalSpeed) {
+  return Number((numberOfOrbits * orbitCircumference(orbitRadius) / orbitalSpeed).toFixed(2))
+}
 
 // Copy/paste your selectRandomEntry function here:
-
+function selectRandomEntry(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
 
 // Code your oxygenExpended function here:
-
+function oxygenExpended(candidate) {
+  let hours = missionDuration(3, 2000, 28000)
+  let o2kg = candidate.o2Used(hours).toFixed(3);
+  return `${candidate.name} will perform the spacewalk, which will last ${hours} hours and require ${o2kg} kg of oxygen.`
+}
 
 // Candidate data & crew array.
 let candidateA = {
-   'name':'Gordon Shumway',
-   'species':'alf',
-   'mass':90,
-   'o2Used':function(hrs){return 0.035*hrs},
-   'astronautID':414
- };
- let candidateB = {
-   'name':'Lassie',
-   'species':'dog',
-   'mass':19.1,
-   'o2Used':function(hrs){return 0.030*hrs},
-   'astronautID':503
- };
- let candidateC = {
-   'name':'Jonsey',
-   'species':'cat',
-   'mass':3.6,
-   'o2Used':function(hrs){return 0.022*hrs},
-   'astronautID':796
- };
- let candidateD = {
-   'name':'Paddington',
-   'species':'bear',
-   'mass':31.8,
-   'o2Used':function(hrs){return 0.047*hrs},
-   'astronautID':291
- };
- let candidateE = {
-   'name':'Pete',
-   'species':'tortoise',
-   'mass':417,
-   'o2Used':function(hrs){return 0.010*hrs},
-   'astronautID':599
- };
- let candidateF = {
-   'name':'Hugs',
-   'species':'ball python',
-   'mass':2.3,
-   'o2Used':function(hrs){return 0.018*hrs},
-   'astronautID':890
- };
- 
- let crew = [candidateA,candidateC,candidateE];
- 
+  'name': 'Gordon Shumway',
+  'species': 'alf',
+  'mass': 90,
+  'o2Used': function(hrs) { return 0.035 * hrs },
+  'astronautID': 414
+};
+let candidateB = {
+  'name': 'Lassie',
+  'species': 'dog',
+  'mass': 19.1,
+  'o2Used': function(hrs) { return 0.030 * hrs },
+  'astronautID': 503
+};
+let candidateC = {
+  'name': 'Jonsey',
+  'species': 'cat',
+  'mass': 3.6,
+  'o2Used': function(hrs) { return 0.022 * hrs },
+  'astronautID': 796
+};
+let candidateD = {
+  'name': 'Paddington',
+  'species': 'bear',
+  'mass': 31.8,
+  'o2Used': function(hrs) { return 0.047 * hrs },
+  'astronautID': 291
+};
+let candidateE = {
+  'name': 'Pete',
+  'species': 'tortoise',
+  'mass': 417,
+  'o2Used': function(hrs) { return 0.010 * hrs },
+  'astronautID': 599
+};
+let candidateF = {
+  'name': 'Hugs',
+  'species': 'ball python',
+  'mass': 2.3,
+  'o2Used': function(hrs) { return 0.018 * hrs },
+  'astronautID': 890
+};
+
+let crew = [candidateA, candidateC, candidateE];
+
+console.log(oxygenExpended(selectRandomEntry(crew)))
