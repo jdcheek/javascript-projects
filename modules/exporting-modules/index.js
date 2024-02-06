908e63ec58c780874d42651775bc4f673a900612
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-02-05 18:46:12 -0600

    examples completed

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,4 @@
-// Import the modules exported from practiceExports.js below:
+// Import the modules exported from practiceExports.js below:re practiceExport } from './practiceExports'
+const practice = require('./practiceExports.js')
+
+console.log(practice.isPalindrome('radar'))
