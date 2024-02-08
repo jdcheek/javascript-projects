8b862a3cd1572d8ead622ba095cf2972ac347e9c
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-02-07 21:06:42 -0600

    chapter 14 studio: part 1

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,11 @@
 
 let launchcode = {
-  
+  organization: 'nonprofit',
+  executiveDirector: 'launchcode',
+  percentageCoolEmployees: '100%',
+  programsOffered: [],
+  launchOutput: function(num) {
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,15 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
-
+describe("Testing launchcode", function() {
   // Write your unit tests here!
-  
+  test('check the value of organization is "nonprofit"', function() {
+    expect(launchcode.organization).toBe('nonprofit');
+  });
+  test('check the value of executiveDirector is "Jeff"', function() {
+    expect(launchcode.executiveDirector).toBe('Jeff');
+  });
+  test('check the value of percentageCoolEmployees is "100%"', function() {
+    expect(launchcode.percentageCoolEmployees).toBe('100%');
+  });
 });
