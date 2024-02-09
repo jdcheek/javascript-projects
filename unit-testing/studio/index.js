e1558914c45730f83fbf0327cfd72f42116406b6
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-02-08 20:49:10 -0600

    chapter 14 studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,12 +1,30 @@
-
 let launchcode = {
-  organization: 'nonprofit',
-  executiveDirector: 'launchcode',
-  percentageCoolEmployees: '100%',
-  programsOffered: [],
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
   launchOutput: function(num) {
+    let output = '';
+    if (num % 2 === 0) {
+      output += 'Launch';
+    } if (num % 3 === 0) {
+      output += 'Code';
+    } if (num % 5 === 0) {
+      if (output) {
+        output += ' Rocks';
+      } else {
+        output += 'Rocks';
+      }
+    }
+    if (!output) {
+      return ("Rutabagas! That doesn't work.");
+    } else if (output === 'Launch Rocks!') {
+      return output + '!CRASH!!!';
+    }
+    return output + '!';
   }
 }
+};
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -6,10 +6,52 @@ describe("Testing launchcode", function() {
   test('check the value of organization is "nonprofit"', function() {
     expect(launchcode.organization).toBe('nonprofit');
   });
+
   test('check the value of executiveDirector is "Jeff"', function() {
     expect(launchcode.executiveDirector).toBe('Jeff');
   });
-  test('check the value of percentageCoolEmployees is "100%"', function() {
-    expect(launchcode.percentageCoolEmployees).toBe('100%');
+
+  test('check the value of percentageCoolEmployees is 100', function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
   });
+
+  test('Should return Web Developement, Data Analysis, and Liftoff.', function() {
+    expect(launchcode.programsOffered[0]).toBe('Web Development');
+    expect(launchcode.programsOffered[1]).toBe('Data Analysis');
+    expect(launchcode.programsOffered[2]).toBe('Liftoff');
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+
+  test('When passed a number that is ONLY divisible by 2, return "Launch!"', function() {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
+
+  test('When passed a number that is ONLY divisible by 3, return "Code!"', function() {
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+  });
+
+  test('When passed a number that is ONLY divisible by 5, return "Rocks!"', function() {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  });
+
+  test('When passed a number that is divisible by 2 AND 3, return "LaunchCode!"', function() {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  });
+
+  test('When passed a number that is divisible by 3 AND 5, return "Code Rocks!"', function() {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  });
+
+  test('When passed a number that is divisible by 2 AND 5, return "Launch Rocks! (CRASH!!!!)"', function() {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!!)");
+  });
+
+  test('When passed a number that is divisible by 2, 3 AND 5, return "LaunchCode Rocks!"', function() {
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+  });
+
+  test('When passed a number that is NOT divisible by 2, 3 or 5, return "Rutabagas! That doesn\'t work."', function() {
+    expect(launchcode.launchOutput(1)).toBe("Rutabagas! That doesn\'t work.");
+  });
+
 });
