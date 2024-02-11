b793ad6411af048941f6108b0bc44cf8a6e5ea99
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-02-10 20:38:27 -0600

    chapter 14 exercises

--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -2,9 +2,13 @@ const checkFive = require('../checkFive.js');
 
 describe("checkFive", function() {
 
-  test("Descriptive feedback...", function() {
+  test("returns 5 is equal to 5", function() {
+    let output = checkFive(5);
+    expect(output).toEqual("5 is equal to 5.");
+  });
+
+  test("returns 2 is equal to 5", function() {
     let output = checkFive(2);
     expect(output).toEqual("2 is less than 5.");
   });
-
 });
