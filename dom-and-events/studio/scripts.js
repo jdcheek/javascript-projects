39cb1b5fdde929d580152b27be640e8389a885a1
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-02-26 20:23:04 -0600

    chapter 23 studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -21,16 +21,22 @@ const missionAbort = document.querySelector('#missionAbort')
 const flightStatus = document.querySelector('#flightStatus')
 const shuttleBackground = document.querySelector('#shuttleBackground')
 const spaceShuttleHeight = document.querySelector('#spaceShuttleHeight')
+const rocket = document.querySelector('#rocket')
 const up = document.querySelector('#up')
 const down = document.querySelector('#down')
 const left = document.querySelector('#left')
 const right = document.querySelector('#right')
+rocket.style.right = '0px'
+rocket.style.left = '340px'
+rocket.style.top = '250px'
+rocket.style.bottom = '0px'
 
 takeOff.addEventListener('click', (event) => {
   if (confirm('Confirm that the shuttle is ready for takeoff')) {
     flightStatus.innerHTML = 'Shuttle in flight.'
     shuttleBackground.style.backgroundColor = 'blue'
     spaceShuttleHeight.innerHTML = '10000'
+    rocket.style.top = (parseInt(rocket.style.top) - 10) + 'px'
   }
 })
 
@@ -39,6 +45,10 @@ landing.addEventListener('click', (event) => {
   flightStatus.innerHTML = 'The shuttle has landed.'
   shuttleBackground.style.backgroundColor = 'green'
   spaceShuttleHeight.innerHTML = '0'
+  rocket.style.right = '0px'
+  rocket.style.left = '340px'
+  rocket.style.top = '250px'
+  rocket.style.bottom = '0px'
 })
 
 missionAbort.addEventListener('click', (event) => {
@@ -46,23 +56,35 @@ missionAbort.addEventListener('click', (event) => {
     flightStatus.innerHTML = 'Mission aborted'
     shuttleBackground.style.backgroundColor = 'green'
     spaceShuttleHeight.innerHTML = '0'
+    rocket.style.right = '0px'
+    rocket.style.left = '340px'
+    rocket.style.top = '250px'
+    rocket.style.bottom = '0px'
   }
 })
 
 up.addEventListener('click', (event) => {
-  // move rocket image up 10px using absolute position
-  // increase shuttle height by 10000
+  if (parseInt(rocket.style.top) > -10) {
+    rocket.style.top = (parseInt(rocket.style.top) - 10) + 'px'
+    spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000
+  }
 })
 
 down.addEventListener('click', (event) => {
-  // move rocket image down 10px using absolute position
-  // decrease shuttle height by 10000
+  if (parseInt(rocket.style.top) < 250) {
+    rocket.style.top = (parseInt(rocket.style.top) + 10) + 'px'
+    spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) - 10000
+  }
 })
 
 left.addEventListener('click', (event) => {
-  // move rocket image left 10px using absolute position
+  if (parseInt(rocket.style.left) > -20) {
+    rocket.style.left = (parseInt(rocket.style.left) - 10) + 'px'
+  }
 })
 
 right.addEventListener('click', (event) => {
-  // move rocket image right 10px using absolute position
+  if (parseInt(rocket.style.left) < 710) {
+    rocket.style.left = (parseInt(rocket.style.left) + 10) + 'px'
+  }
 })
